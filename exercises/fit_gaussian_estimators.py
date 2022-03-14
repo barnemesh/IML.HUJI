38d67d0bdb926664be4e5f0c41643330c651dc14
Bar Nemesh <bar.nemesh@cs.huji.ac.il>
2022-03-14 23:21:13 +0200

    added varius types of plots for multivariate graphs in the run_exercises_test.ipynb. still not checked.

--- exercises/fit_gaussian_estimators.py
+++ exercises/fit_gaussian_estimators.py
@@ -27,17 +27,18 @@ def test_univariate_gaussian():
 
     # TODO: GO: plotly.graph_objects
     figure = px.line(x=[i * 10 for i in range(1, 101)], y=expectations,
-                     labels=dict(x="Sample size", y="Distance from estimated to true expectation"))
-    # figure.show()
-    plotly.offline.plot(figure, filename="temp-plot-1.html")
+                     labels=dict(x="Sample size",
+                                 y="Distance from estimated to true expectation"))
+    figure.show()
+    # plotly.offline.plot(figure, filename="temp-plot-1.html")
 
     # Question 3 - Plotting Empirical PDF of fitted model
     # last fit was 1000: same as Q1
     figure2 = px.scatter(x=samples, y=uni.pdf(samples),
                          labels=dict(x="Value of sample", y="PDF of sample"))
 
-    plotly.offline.plot(figure2)
-    # figure2.show()
+    # plotly.offline.plot(figure2)
+    figure2.show()
 
 
 def test_multivariate_gaussian():
@@ -75,22 +76,27 @@ def test_multivariate_gaussian():
     )
     # zer = np.zeros(200)
     # pairs_f1_f3 = np.transpose(np.stack([values_for_mu, zer, values_for_mu, zer]))
-    print(pairs_f1_f3)
-    multi_loglikely = lambda m: MultivariateGaussian.log_likelihood(m, sigma, samples)
+
+    multi_loglikely = lambda m: MultivariateGaussian.log_likelihood(m, sigma,
+                                                                    samples)
     log_likelihoods = np.apply_along_axis(multi_loglikely, 1, pairs_f1_f3)
 
-    figure2 = px.density_heatmap(x=pairs_f1_f3[:, 0], y=pairs_f1_f3[:, 2], z=log_likelihoods,
-                                 labels=dict(x="value of f1", y="value of f3", z="calculated log-likelihood"),
+    figure2 = px.density_heatmap(x=pairs_f1_f3[:, 0], y=pairs_f1_f3[:, 2],
+                                 z=log_likelihoods,
+                                 labels=dict(x="value of f1", y="value of f3",
+                                             z="calculated log-likelihood"),
                                  title="Log-likelihood using mu = [f1,0,f3,0]^T as expectation",
                                  )
-    # figure2.show()
-    plotly.offline.plot(figure2, filename="temp-plot-multi.html")
+    figure2.show()
+    # plotly.offline.plot(figure2, filename="temp-plot-multi.html")
 
     # Question 6 - Maximum likelihood
-    print(pairs_f1_f3[np.argmax(log_likelihoods), [0,2]])
+    print("{:.3f} , {:.3f}".format(
+        *pairs_f1_f3[np.argmax(log_likelihoods), [0, 2]])
+    )
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-    # test_univariate_gaussian()
+    test_univariate_gaussian()
     test_multivariate_gaussian()
