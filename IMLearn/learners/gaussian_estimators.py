Changed to actual log-likelihood, fixed graphs

IMLearn/learners/gaussian_estimators.py
@@ -86,11 +86,7 @@ class UnivariateGaussian:
             raise ValueError(
                 "Estimator must first be fitted before calling `pdf` function")
 
-        def univariate_gaussian_pdf(x):
-            return np.exp(-((x - self.mu_) ** 2) / (2 * self.var_)) \
-                   / np.sqrt(2 * np.pi * self.var_)
-
-        return univariate_gaussian_pdf(X)
+        return np.exp(-((X - self.mu_) ** 2) / (2 * self.var_)) / np.sqrt(2 * np.pi * self.var_)
 
     @staticmethod
     def log_likelihood(mu: float, sigma: float, X: np.ndarray) -> float:
@@ -111,11 +107,11 @@ class UnivariateGaussian:
         log_likelihood: float
             log-likelihood calculated
         """
-
-        def log_likelihood(x):
-            return (x - mu) ** 2
-
-        return -X.shape[0] * np.log(sigma) - np.sum(log_likelihood(X)) / sigma
+        # As mentioned in the forum: we can ignore the constants, to get an
+        # easier to calculate estimator:
+        # return -X.shape[0] * np.log(sigma) - np.sum((X - mu) ** 2) / sigma
+        # The "Real" full log-likelihood:
+        return (-X.shape[0] / 2) * np.log(2 * np.pi * sigma) - np.sum((X - mu) ** 2) / (2 * sigma)
 
 
 class MultivariateGaussian:
@@ -219,8 +215,15 @@ class MultivariateGaussian:
             log-likelihood calculated over all input data and under given parameters of Gaussian
         """
         inv_cov = np.linalg.inv(cov)
+        n_samples, n_features = X.shape
 
         def log_likelihood(x):
             return (x - mu).T @ inv_cov @ (x - mu)
 
-        return -X.shape[0] * np.log(np.linalg.det(cov)) - np.sum(np.apply_along_axis(log_likelihood, 0, X.T))
+        # As mentioned in the forum: we can ignore the constants, to get an
+        # easier to calculate estimator:
+        # return -n_samples * np.log(np.linalg.det(cov)) - np.sum(np.apply_along_axis(log_likelihood, 0, X.T))
+        # The "Real" full log-likelihood:
+        return (-n_samples / 2) * np.log(
+            ((2 * np.pi) ** n_features) * np.linalg.det(cov)) - np.sum(
+            np.apply_along_axis(log_likelihood, 0, X.T)) / 2

exercises/fit_gaussian_estimators.py
@@ -76,16 +76,17 @@ def test_multivariate_gaussian():
     fig = go.Figure(
         go.Heatmap(y=pairs_f1_f3[:, 0],
                    x=pairs_f1_f3[:, 2],
-                   z=log_likelihoods
+                   z=log_likelihoods,
+                   colorbar={"title": "Value of Log-Likelihood"}
                    )
     )
     fig.update_layout(
-        title="Log-likelihood using mu = [f1,0,f3,0]^T as expectation",
+        title="Log-likelihood using mu=[f1,0,f3,0]^T as expectation, sampled from [-10,10]",
         yaxis_title="value of f1",
         xaxis_title="value of f3",
-        legend_title="calculated log-likelihood"
     )
     fig.show()
+    plotly.offline.plot(fig, filename="first.html")
 
     figure2 = px.density_heatmap(y=pairs_f1_f3[:, 0],
                                  x=pairs_f1_f3[:, 2],
@@ -97,13 +98,13 @@ def test_multivariate_gaussian():
                                  histnorm="density"
                                  )
     figure2.show()
-
+    plotly.offline.plot(figure2, filename="second.html")
     # Question 6 - Maximum likelihood
     mle_index = np.argmax(log_likelihoods)
     print("f1: {:.3f} , f3: {:.3f}".format(
         *pairs_f1_f3[mle_index, [0, 2]])
     )
-    print(f"MLE: {log_likelihoods[mle_index]}")
+    print(f"Log-Likelihood: {log_likelihoods[mle_index]:.3f}")
 
 
 if __name__ == '__main__':