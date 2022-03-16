Added m to log-likelihood, fixed figure shape in Q3.2.2, need to choose figure from 2 possibilities, some prints and optimizations

IMLearn/learners/gaussian_estimators.py
@@ -111,9 +111,11 @@ class UnivariateGaussian:
         log_likelihood: float
             log-likelihood calculated
         """
+
         def log_likelihood(x):
             return (x - mu) ** 2
-        return -np.sum(log_likelihood(X)) / sigma  # TODO: divide by sigma?
+
+        return -X.shape[0] * np.log(sigma) - np.sum(log_likelihood(X)) / sigma
 
 
 class MultivariateGaussian:
@@ -160,13 +162,6 @@ class MultivariateGaussian:
         Sets `self.mu_`, `self.cov_` attributes according to calculated estimation.
         Then sets `self.fitted_` attribute to `True`
         """
-        # n_samples, n_features = X.shape
-        # uni = UnivariateGaussian()
-        # get_univariate_mu = lambda x: uni.fit(x).mu_
-        # self.mu_ = np.apply_along_axis(get_univariate_mu, 0, X)
-        #
-        # x_hat = X - self.mu_
-        # self.cov_ = (x_hat.T @ x_hat) / (n_samples - 1)
         self.cov_ = np.cov(X.T, ddof=1)
         self.mu_ = np.mean(X, axis=0)
 
@@ -223,6 +218,9 @@ class MultivariateGaussian:
         log_likelihood: float
             log-likelihood calculated over all input data and under given parameters of Gaussian
         """
+        inv_cov = np.linalg.inv(cov)
+
         def log_likelihood(x):
-            return (x - mu).T @ np.linalg.inv(cov) @ (x - mu)
-        return -np.sum(np.apply_along_axis(log_likelihood, 0, X.T))
+            return (x - mu).T @ inv_cov @ (x - mu)
+
+        return -X.shape[0] * np.log(np.linalg.det(cov)) - np.sum(np.apply_along_axis(log_likelihood, 0, X.T))

exercises/fit_gaussian_estimators.py
@@ -17,6 +17,7 @@ def test_univariate_gaussian():
     samples = np.random.normal(10, 1, 1000)
 
     uni.fit(samples)
+    print("(expectation, variance)")
     print(f"({uni.mu_}, {uni.var_})")
 
     # Question 2 - Empirically showing sample mean is consistent
@@ -25,19 +26,18 @@ def test_univariate_gaussian():
         uni.fit(samples[:i * 10])
         expectations[i - 1] = np.abs(uni.mu_ - 10)
 
-    # TODO: GO: plotly.graph_objects
     figure = px.line(x=[i * 10 for i in range(1, 101)], y=expectations,
+                     title="Distance from estimated to true expectation based on sample size",
                      labels=dict(x="Sample size",
                                  y="Distance from estimated to true expectation"))
     figure.show()
-    # plotly.offline.plot(figure, filename="temp-plot-1.html")
 
     # Question 3 - Plotting Empirical PDF of fitted model
     # last fit was 1000: same as Q1
     figure2 = px.scatter(x=samples, y=uni.pdf(samples),
+                         title="Calculated PDF of the samples in Univariate gaussian estimator",
                          labels=dict(x="Value of sample", y="PDF of sample"))
 
-    # plotly.offline.plot(figure2)
     figure2.show()
 
 
@@ -52,10 +52,10 @@ def test_multivariate_gaussian():
     multi = MultivariateGaussian()
     samples = numpy.random.multivariate_normal(mu, sigma, 1000)
     multi.fit(samples)
+    print("Estimated expectation")
     print(multi.mu_)
+    print("Unbiased estimated Covariance matrix")
     print(multi.cov_)
-
-    # plotly.offline.plot(figure2, filename="temp-plot-multi.html")
 
     # Question 5 - Likelihood evaluation
     values_for_mu = np.linspace(-10, 10, 200)
@@ -67,29 +67,43 @@ def test_multivariate_gaussian():
                   np.zeros(len(rep))
                   ])
     )
-    # zer = np.zeros(200)
-    # pairs_f1_f3 = np.transpose(np.stack([values_for_mu, zer, values_for_mu, zer]))
 
     def multi_loglikely(m):
         return MultivariateGaussian.log_likelihood(m, sigma, samples)
 
     log_likelihoods = np.apply_along_axis(multi_loglikely, 1, pairs_f1_f3)
 
-    figure2 = px.density_heatmap(x=pairs_f1_f3[:, 0], y=pairs_f1_f3[:, 2],
+    fig = go.Figure(
+        go.Heatmap(y=pairs_f1_f3[:, 0],
+                   x=pairs_f1_f3[:, 2],
+                   z=log_likelihoods
+                   )
+    )
+    fig.update_layout(
+        title="Log-likelihood using mu = [f1,0,f3,0]^T as expectation",
+        yaxis_title="value of f1",
+        xaxis_title="value of f3",
+        legend_title="calculated log-likelihood"
+    )
+    fig.show()
+
+    figure2 = px.density_heatmap(y=pairs_f1_f3[:, 0],
+                                 x=pairs_f1_f3[:, 2],
                                  z=log_likelihoods,
-                                 labels=dict(x="value of f1", y="value of f3",
+                                 labels=dict(y="value of f1", x="value of f3",
                                              z="calculated log-likelihood"),
-                                 title="Log-likelihood using mu = [f1,0,f3,0]^T as expectation",
+                                 title="Log-likelihood using mu=[f1,0,f3,0]^T as expectation, sampled from [-10,10]",
                                  histfunc="avg",
                                  histnorm="density"
                                  )
     figure2.show()
-    # plotly.offline.plot(figure2, filename="temp-plot-multi.html")
 
     # Question 6 - Maximum likelihood
-    print("{:.3f} , {:.3f}".format(
-        *pairs_f1_f3[np.argmax(log_likelihoods), [0, 2]])
+    mle_index = np.argmax(log_likelihoods)
+    print("f1: {:.3f} , f3: {:.3f}".format(
+        *pairs_f1_f3[mle_index, [0, 2]])
     )
+    print(f"MLE: {log_likelihoods[mle_index]}")
 
 
 if __name__ == '__main__':