aa68f37d4e2affb55dc791c23efd830292827860
Bar Nemesh <barnemesh@gmail.com>
2022-03-29 22:41:39 +0300

    Challenge work. attempts at preproccessing

--- challenge/agoda_cancellation_prediction.py
+++ challenge/agoda_cancellation_prediction.py
@@ -1,11 +1,17 @@
+import sklearn.neighbors
+
 from challenge.agoda_cancellation_estimator import AgodaCancellationEstimator
 # from IMLearn.base import BaseEstimator
 from IMLearn.utils import split_train_test
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
 import datetime as dt
+import re
 
 
 def load_data(filename: str):
@@ -31,12 +37,33 @@ def load_data(filename: str):
                                          "checkout_date",
                                          "hotel_live_date"]).drop_duplicates()
     full_data["cancellation_datetime"] = full_data["cancellation_datetime"].notna()
-    full_data.dropna()
-    full_data["booking_datetime"] = full_data["booking_datetime"].map(dt.datetime.toordinal).fillna(0)
-    full_data["checkin_date"] = full_data["checkin_date"].map(dt.datetime.toordinal).fillna(0)
-    full_data["checkout_date"] = full_data["checkout_date"].map(dt.datetime.toordinal).fillna(0)
-    full_data["hotel_live_date"] = full_data["hotel_live_date"].map(dt.datetime.toordinal).fillna(0)
-    features = full_data.drop(["hotel_country_code",
+    full_data = full_data.drop([
+        "request_nonesmoke",
+        "request_latecheckin",
+        "request_highfloor",
+        "request_largebed",
+        "request_twinbeds",
+        "request_airport",
+        "request_earlycheckin",
+        "hotel_chain_code",
+        "hotel_chain_code"
+    ], axis=1)
+    print(full_data.shape)
+    full_data = full_data.dropna()
+    full_data['TimeDiff'] = (full_data['checkin_date'] - full_data['booking_datetime']).dt.days
+    full_data["cancellation_policy_numbered"] = \
+        full_data.apply(lambda x: transform_policy(x["cancellation_policy_code"],
+                                                   x["TimeDiff"],
+                                                   x["original_selling_amount"]), axis=1)
+    full_data["booking_datetime"] = full_data["booking_datetime"].map(dt.datetime.toordinal)  # .fillna(0)
+    full_data["checkin_date"] = full_data["checkin_date"].map(dt.datetime.toordinal)  # .fillna(0)
+    full_data["checkout_date"] = full_data["checkout_date"].map(dt.datetime.toordinal)  # .fillna(0)
+    full_data["hotel_live_date"] = full_data["hotel_live_date"].map(dt.datetime.toordinal)  # .fillna(0)
+    features = full_data.drop(["cancellation_datetime",
+                               "checkout_date",
+                               "checkin_date",
+                               "booking_datetime",
+                               "hotel_country_code",
                                "accommadation_type_name",
                                "cancellation_policy_code",
                                "charge_option",
@@ -46,19 +73,19 @@ def load_data(filename: str):
                                "origin_country_code",
                                "original_payment_type",
                                "original_payment_method",
-                               "original_payment_type",
-                               "original_payment_currency"
+                               "original_payment_currency",
+                               "h_booking_id"
                                ], axis=1)
-    # features = full_data[[
-    #     "booking_datetime",
-    #     "checkin_date",
-    #     "checkout_date",
-    #     "hotel_live_date",
-    #     "is_first_booking",
-    #     "hotel_star_rating",
-    #     "no_of_ad"]]
     labels = full_data["cancellation_datetime"]
-
+    features = full_data[[
+        "TimeDiff",
+        "cancellation_policy_numbered",
+        "hotel_star_rating",
+        "no_of_children",
+        "no_of_adults",
+        "original_selling_amount",
+        "is_first_booking"
+    ]]
     return features, labels
 
 
@@ -68,25 +95,86 @@ def load_test(filename: str):
                                          "checkin_date",
                                          "checkout_date",
                                          "hotel_live_date"]).drop_duplicates()
-    full_data.dropna()
-    full_data["booking_datetime"] = full_data["booking_datetime"].map(dt.datetime.toordinal).fillna(0)
-    full_data["checkin_date"] = full_data["checkin_date"].map(dt.datetime.toordinal).fillna(0)
-    full_data["checkout_date"] = full_data["checkout_date"].map(dt.datetime.toordinal).fillna(0)
-    full_data["hotel_live_date"] = full_data["hotel_live_date"].map(dt.datetime.toordinal).fillna(0)
-    features = full_data.drop(["hotel_country_code",
-                               "accommadation_type_name",
-                               "cancellation_policy_code",
-                               "charge_option",
-                               "customer_nationality",
-                               "guest_nationality_country_name",
-                               "language",
-                               "origin_country_code",
-                               "original_payment_type",
-                               "original_payment_method",
-                               "original_payment_type",
-                               "original_payment_currency"
-                               ], axis=1)
+    full_data = full_data.drop([
+        "request_nonesmoke",
+        "request_latecheckin",
+        "request_highfloor",
+        "request_largebed",
+        "request_twinbeds",
+        "request_airport",
+        "request_earlycheckin",
+        "hotel_chain_code",
+        "hotel_chain_code"
+    ], axis=1)
+    full_data = full_data.dropna()
+    full_data['TimeDiff'] = (full_data['checkin_date'] - full_data['booking_datetime']).dt.days
+    full_data["cancellation_policy_numbered"] = \
+        full_data.apply(lambda x: transform_policy(x["cancellation_policy_code"],
+                                                   x["TimeDiff"],
+                                                   x["original_selling_amount"]), axis=1)
+    full_data["booking_datetime"] = full_data["booking_datetime"].map(dt.datetime.toordinal)  # .fillna(0)
+    full_data["checkin_date"] = full_data["checkin_date"].map(dt.datetime.toordinal)  # .fillna(0)
+    full_data["checkout_date"] = full_data["checkout_date"].map(dt.datetime.toordinal)  # .fillna(0)
+    full_data["hotel_live_date"] = full_data["hotel_live_date"].map(dt.datetime.toordinal)  # .fillna(0)
+    features = full_data.drop([
+        "checkout_date",
+        "checkin_date",
+        "booking_datetime",
+        "hotel_country_code",
+        "accommadation_type_name",
+        "cancellation_policy_code",
+        "charge_option",
+        "customer_nationality",
+        "guest_nationality_country_name",
+        "language",
+        "origin_country_code",
+        "original_payment_type",
+        "original_payment_method",
+        "original_payment_currency",
+        "h_booking_id"
+    ], axis=1)
+    features = full_data[[
+        "TimeDiff",
+        "cancellation_policy_numbered",
+        "hotel_star_rating",
+        "no_of_children",
+        "no_of_adults",
+        "original_selling_amount",
+        "is_first_booking"
+    ]]
     return features
+
+
+regex = r"""([\d])([D|P])([\d])([N|P])"""
+
+
+def transform_policy(policy, nights, cost):
+    matches = re.findall(regex, policy)
+
+    result = 0
+
+    for match in matches:
+        if len(match) == 2:
+            result += (int(match[0]) / 100) * cost
+        else:
+            if match[0] == "0":
+                divider = 1
+            else:
+                divider = int(match[0])
+
+            if match[3] == 'N':
+
+                if nights == 0:
+                    nights_divider = 1
+                else:
+                    nights_divider = nights
+
+                result += (1 / divider) * (int(match[2]) / nights_divider) * cost
+
+            else:
+                result += (1 / divider) * (int(match[2]) / 100) * cost
+
+    return result
 
 
 def evaluate_and_export(estimator  #: BaseEstimator,
@@ -121,18 +209,24 @@ if __name__ == '__main__':
 
     # Fit model over data
     # estimator = AgodaCancellationEstimator().fit(train_X, train_y)
-    model = LogisticRegression(max_iter=1000)
-    estimator = model.fit(train_X, train_y)
+    # model = KNeighborsClassifier()
+    model = LogisticRegression(max_iter=100000)
+    # pipe = make_pipeline(StandardScaler(), model)
+    # estimator = pipe.fit(train_X, train_y)  # apply scaling on training data
+    model.fit(train_X, train_y)
     predictions = model.predict(test_X)
-    acc = predictions == test_y
-    print(acc.value_counts())
-    print(accuracy_score(test_y, predictions))
+    print(model.score(test_X, test_y))
 
     std_y = np.std(responses)
     for name, values in df.items():
         array = values.to_numpy()
         p_cor = np.cov(array, responses)[0, 1] / (np.std(array) * std_y)
-        print(p_cor)
+        print(f"{name} : {p_cor}")
+
+    print("coef:")
+
+    for name, coef in zip(model.feature_names_in_, np.transpose(model.coef_)):
+        print(f"{name}: {coef}")
     # Store model predictions over test set
     real = load_test("../datasets/test_set_week_1.csv")
-    evaluate_and_export(estimator, real, "id1_id2_id3.csv")
+    evaluate_and_export(model, real, "id1_id2_id3.csv")
