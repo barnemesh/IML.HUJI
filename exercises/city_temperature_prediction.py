ab2a074f51bd3ce9d9b7c8169b6e453b78aa011d
Bar Nemesh <barnemesh@gmail.com>
2022-03-31 21:08:27 +0300

    Finished Polyfit and base work on city_temperature_prediction.py is done.

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -69,10 +69,93 @@ if __name__ == '__main__':
     # TODO: better in months with lower STD?
 
     # Question 3 - Exploring differences between countries
-    px.line()
+    cm_df = df.groupby(["Country", "Month"])["Temp"].agg(
+        ["mean", "std"]).reset_index()
+    plot = px.line(
+        data_frame=cm_df,
+        x="Month",
+        y="mean",
+        error_y="std",
+        title="average monthly temperature",
+        color="Country",
+        labels={"MeanTemp": "Mean of Temp", "StdTemp": "Std of Temp"}
+    )
+    plot.show()
+    plot.write_image("./Plots/AvgTemp.png")  # TODO : remove
+    # TODO: SA has a different pattern, will not do good with the IL model.
+    # TODO: NTL will also not do as good as JRD - the model is likely to have
+    #  strong bias.
 
     # Question 4 - Fitting model for different values of `k`
-    raise NotImplementedError()
+    loss = []
+    train_X, train_y, test_X, test_y = split_train_test(
+        il_df["DayOfYear"], il_df["Temp"]
+    )
+    test_X, test_y = test_X.to_numpy(), test_y.to_numpy()
+    for k in range(1, 11):
+        model = PolynomialFitting(k)
+        model.fit(train_X.to_numpy(), train_y.to_numpy())
+        loss.append(
+            [k,
+             np.round(model.loss(test_X, test_y), 2),
+             # np.mean((test_y-y_hat)**2)
+             ])
+        print("deg {} : loss {}".format(*loss[-1]))
+
+    loss_df = pd.DataFrame.from_records(loss, columns=["k", "loss"])
+    plot = px.bar(data_frame=loss_df,
+                  x="k",
+                  y="loss",
+                  title="Test error recorded based on degree of Polyfit k",
+                  labels={"loss": "Test error recorded"})
+    plot.show()
+    plot.write_image("./Plots/PolyfitError.png")  # TODO : remove
+    # TODO: 5 or 6 are the best - 6 slightly better, marginal, but 5 less
+    #  complex. higher values will cause over fit -
+    #  we see that 9 and 10 already have the same loss, so adding higher k will
+    #  only complicate the model and will not get better results.
 
     # Question 5 - Evaluating fitted model on different countries
-    raise NotImplementedError()
+    model = PolynomialFitting(5)
+    model.fit(il_df["DayOfYear"], il_df["Temp"])
+    no_il_df = df.drop(df[df["Country"] == "Israel"].index)
+    ctr_df = [y for x, y in no_il_df.groupby(["Country"])]
+    ctr_loss = []
+    for c in ctr_df:
+        X = c["DayOfYear"]
+        y = c["Temp"]
+        country = c["Country"].unique()[0]
+        ctr_loss.append(
+            [country,
+             np.round(model.loss(X, y), 2),
+             # np.mean((test_y-y_hat)**2)
+             ])
+        # TODO: remove
+        plot = px.scatter(
+            x=X,
+            y=[y, model.predict(X)],
+            title=f"{country} predict and true",
+        ).write_image(f"./Plots/{country}Predict.png")
+
+    ctr_loss_df = pd.DataFrame.from_records(ctr_loss,
+                                            columns=["Country", "Loss"])
+    plot = px.bar(data_frame=ctr_loss_df,
+                  x="Country",
+                  y="Loss",
+                  title="Error of model fitted over Israel by country",
+                  labels={"Loss": "Test error recorded"},
+                  text="Loss",
+                  color="Country")
+    plot.show()
+    plot.write_image("./Plots/CountryPolyfit.png")
+    # TODO: we have great fit in jordan - that had very similar cross with the
+    #  area of the israel data, but with the netherlands, even if the
+    #  polynomial is in similar shape, we had consistent bias, and that
+    #  increased our loss. In south africa we had the opposite mistake -
+    #  since some of the data did match, the loss was lower, even when the
+    #  the polynomial was completely wrong - as we saw in the months that had
+    #  some cross in the values .
+    #  summary:
+    #  netherlands : we saw no crossing with israel, big loss.
+    #  jordan : lots of cross, big good.
+    #  south africa : some cross, some not, not so good, misleading results!
