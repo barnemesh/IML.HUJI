f392f7741348d5fe0f797fe90053a0ef35aea97d
Bar Nemesh <barnemesh@gmail.com>
2022-05-22 14:50:32 +0300

    Q2.2 base complete

--- exercises/perform_model_selection.py
+++ exercises/perform_model_selection.py
@@ -33,24 +33,25 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
 
     samples = np.linspace(-1.2, 2, n_samples)
     labels = f(samples)
-    eps = np.random.default_rng().normal(loc=0, scale=noise, size=n_samples)
+    eps = np.random.normal(loc=0, scale=noise, size=n_samples)
     labels += eps
 
     X_train, y_train, X_test, y_test = split_train_test(pd.DataFrame(samples), pd.Series(labels), 2.0 / 3.0)
-    # X_train, y_train, X_test, y_test = split_train_test(samples.reshape(-1, 1), labels, 2.0/3.0)
+    X_train, y_train = X_train.to_numpy()[:, 0], y_train.to_numpy()
+    X_test, y_test = X_test.to_numpy()[:, 0], y_test.to_numpy()
 
     fig = go.Figure(
         data=[
             go.Scatter(
-                x=X_train.to_numpy()[:, 0],
-                y=f(X_train.to_numpy()[:, 0]),
+                x=X_train,
+                y=f(X_train),
                 mode="markers",
                 name="Train Samples",
                 marker=dict(color=custom[0][0])
             ),
             go.Scatter(
-                x=X_test.to_numpy()[:, 0],
-                y=f(X_test.to_numpy()[:, 0]),
+                x=X_test,
+                y=f(X_test),
                 mode="markers",
                 name="Test Samples",
                 marker=dict(color=custom[0][-1])
@@ -62,13 +63,52 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
             yaxis=dict(title="Noiseless")
         )
     )
-    fig.write_image(f"./Plots/Ex5/TrueNoiselessModelSplit.png")
+    fig.write_image(f"./Plots/Ex5/TrueNoiselessModelSplitNoise{noise}.png")
 
     # Question 2 - Perform CV for polynomial fitting with degrees 0,1,...,10
-    raise NotImplementedError()
+    train_errors = []
+    validation_errors = []
+    for k in range(11):
+        est = PolynomialFitting(k)
+        t_error, v_error = cross_validate(est, X_train, y_train, mean_square_error)
+        train_errors.append(t_error)
+        validation_errors.append(v_error)
+
+    fig = go.Figure(
+        data=[
+            go.Scatter(
+                x=[k for k in range(11)],
+                y=train_errors,
+                mode="markers",
+                name="Train Error Average",
+                marker=dict(color=custom[0][0])
+            ),
+            go.Scatter(
+                x=[k for k in range(11)],
+                y=validation_errors,
+                mode="markers",
+                name="Validation Error Average",
+                marker=dict(color=custom[0][-1])
+            )
+        ],
+        layout=go.Layout(
+            title="Q2.1.2",
+            xaxis=dict(title="K deg of polynomial"),
+            yaxis=dict(title="MSE")
+        )
+    )
+    fig.write_image(f"./Plots/Ex5/Polynomial5FoldErrorsNoise{noise}.png")
 
     # Question 3 - Using best value of k, fit a k-degree polynomial model and report test error
-    raise NotImplementedError()
+    k = np.argmin(validation_errors)
+    est = PolynomialFitting(k)
+    est.fit(X_train, y_train)
+    test_error = mean_square_error(y_test, est.predict(X_test))
+    print(f"\nFor n_samples={n_samples}, noise={noise}:")
+    print(f"Minimum validation error was for deg {k}.")
+    print(f"Deg {k} MSE: {np.round(test_error, 2)}.")
+    print(f"Deg {k} Validation MSE: {np.round(validation_errors[k], 2)}.")
+
 
 
 def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 500):
@@ -97,3 +137,7 @@ def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 50
 if __name__ == '__main__':
     np.random.seed(0)
     select_polynomial_degree()
+    np.random.seed(0)
+    select_polynomial_degree(noise=0)
+    np.random.seed(0)
+    select_polynomial_degree(n_samples=1500, noise=10)
